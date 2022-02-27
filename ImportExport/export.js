b0cb75f0ca928f04cde39b233e3757df6798384a
Amrit Raj <amritraj.cse19@chitkarauniversity.edu.in>
2022-02-27 11:45:05 +0530

    ImportExport

---
+++ ImportExport/export.js
@@ -0,0 +1,16 @@
+
+const app = (a,b)=>{
+    return a+b;
+}
+
+const name = "Amrit Raj"
+
+
+// if there is a single export
+
+// module.exports = app;
+
+// If there is a multiple export
+
+module.exports = {app,name};
+
---
+++ ImportExport/import.js
@@ -0,0 +1,5 @@
+const {app,name} = require("./export");
+
+console.log(app(5,5));
+
+console.log(name);
