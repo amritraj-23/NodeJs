6d0968b98182119d061e4146cd749d70e20dca63
Amrit Raj <amritraj.cse19@chitkarauniversity.edu.in>
2022-02-27 11:43:57 +0530

    Path module

---
+++ Path/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+// to get the dir name
+
+console.log(path.dirname("C:/Users/dell/Desktop/Web Development/NodeJs/Path/index.js"));
+
+
+// to get the extension name
+console.log(path.extname('C:\Users\dell\Desktop\Web Development\NodeJs\Path\index.js'));
+
+// to get absolute path
+
+console.log(__dirname);
